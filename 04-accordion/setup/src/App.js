47ecd01b85af41c877852c124d37383e95ddb792
Ahmed Dridi <ahmeddridi.1234@gmail.com>
2021-09-09 02:46:56 +0100

    project finished : Accordion,Menu

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,20 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+
+  return (
+  <>
+    <div className='container'>
+      <h3>question and answers about login</h3>
+      <section className='info'>
+         {data.map((question)=>{
+          return<SingleQuestion key={question.id} question={question}/>
+        })}
+      </section>
+    </div>
+  
+  </>
+  );
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = (props) => {
+  const {id, title, info} = props.question
+  const [clicked,setClicked]= useState(false);
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={()=>setClicked(!clicked)}>
+          {clicked? <AiOutlineMinus/> : <AiOutlinePlus/>}
+        </button>
+      </header>
+      {clicked && <p>{info}</p>}
+    </article>
+      
+    
+  
+    );
 };
 
 export default Question;
